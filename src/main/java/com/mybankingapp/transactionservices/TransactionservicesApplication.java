914143acee06fcc02de5f293bc54e3d993d1efef
camilobr89 <camilobr89@hotmail.com>
2025-06-23 14:20:04 -0500

    Initialize transaction services application with Spring Boot, Docker, and Maven setup

---
+++ src/main/java/com/mybankingapp/transactionservices/TransactionservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mybankingapp.transactionservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybankingapp/transactionservices/TransactionservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybankingapp.transactionservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
